6c6fcb34328f9eaa220f6d5ff810793265e878aa
Saran Barua <57630124+saranbarua@users.noreply.github.com>
2022-06-27 12:47:47 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose= require('mongoose')
 const shortUrl = require("./shortUrl")
 
 mongoose.connect(
-    "mongodb+srv://sarandemo:as4wSNDeNl4HO9xB@cluster0.swixu.mongodb.net/urlshortener?retryWrites=true&w=majority",
+    "mongodb+srv://write your name and password B@cluster0.swixu.mongodb.net/urlshortener?retryWrites=true&w=majority",
     () => {
       console.log("connected");
     },
